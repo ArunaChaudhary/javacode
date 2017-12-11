d9c16936502462de4c8c7660e19ca35819e8c7c7
Kay Thi <kaythitun.mdy@gmail.com>
2017-12-11 01:25:04 -0800

    First commit

---
+++ src/main/java/com/fit/project/java_project/SampleJavaProjectApplication.java
@@ -0,0 +1,12 @@
+package com.fit.project.java_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJavaProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fit/project/java_project/SampleJavaProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fit.project.java_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleJavaProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
